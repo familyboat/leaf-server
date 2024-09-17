ee0734b15be0f3b75b4e2dd9560616aafa2b40ad
familyboat <2015301110129@whu.edu.cn>
2024-09-17 17:19:37 +0800

    查看叶子不需要登录

--- routes/leaf.ts
+++ routes/leaf.ts
@@ -5,7 +5,7 @@ import { createLeaf, getLeaves } from "../controllers/leaf.ts";
 const router = new Hono();
 
 router.post('/leaves', authenticate, createLeaf);
-router.get('/leaves', authenticate, getLeaves);
+router.get('/leaves', getLeaves);
 
 const leafRoutes = new Hono().route('/v1', router);
 export default leafRoutes;
